162cf7913d22c999dd2720370925f28c386b9f2d
mycamach <manoli.camacho.a@gmail.com>
2017-09-20 15:36:52 -0300

    changed entire menu

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,67 +1,86 @@
+def show_stock_by_product
+  lines = File.open('productos.txt', 'r').readlines
+  new_lines = []
+  lines.each { |line| new_lines.push(line.split(', ').map(&:chomp)) }
+
+  new_lines.each do |details|
+    name = details.shift
+    sum = get_sum(details)
+    puts "El stock del producto #{name} es #{sum}"
+  end
+end
+
+def get_sum(details)
+  sum = 0
+  details.each do |ele|
+    unless ele == 'NR'
+      sum += ele.to_i
+    end
+  end
+  return sum
+end
+
+option = 0
+
 puts 'Sistema de inventario', "\n"
 
-def menu_inventario
+while option
+  puts 'Ingrese:', "\n"
+  puts '1 para mostrar la cantidad de productos existentes.'
+  puts '2 para mostrar la cantidad de stock de un producto.'
+  puts '3 para mostrar los productos no registrados.'
+  puts '4 para mostrar los productos con un stock menor al ingresado.'
+  puts '5 para registrar un nuevo producto.'
+  puts '6 para salir.'
 
-input = 0
+  option = gets.to_i
 
-while menu
-  puts 'Ingresa 1 para mostrar la cantidad de productos existentes.'
+  case option
 
-while sub_menu
-    puts 'Ingresa a'
-    puts 'Ingresa b'
-    puts 'Ingresa c'
-    puts 'Ingresa d'
+  when 1
+    second_option = 0
     
-    input_2 = gets.chomp.to_s
-    puts case sub_menu
-         when 'a'
-           puts 'a'
-         when 'b'
-           puts 'b'
-         when 'c'
-           puts 'c'
-         when 'd'
-           exit
-         else
-           puts 'Opción no válida'
-           menu_inventario
-         end
+    puts 'Elija opcion a, b, c o d. Presione x para salir.', "\n"
+
+    while second_option
+      puts "a) Mostrar la existencia por productos.\n"
+      puts "b) Mostrar la existencia total por tienda.\n"
+      puts "c) Mostrar la existencia total en todas las tiendas.\n"
+      puts "d) Volver al menú principal.\n"
+
+      second_option = gets.chomp
+
+      case second_option
+      when 'a'
+        puts 'Eligió a.', "\n"
+      when 'b'
+        puts 'Eligió b.', "\n"
+      when 'c'
+        puts 'Eligió c.', "\n"
+      when 'd'
+        exit
+      else
+        puts 'La opción no es valida vuelva a intentarlo.', "\n"
+      end
+    end
+
+  when 2
+    puts '2', "\n"
+
+  when 3
+    puts '3', "\n"
+
+  when 4
+    puts '4', "\n"
+
+  when 5
+    puts '5', "\n"
+
+  when 6
+    puts 'Salir', "\n"
+    exit
+
+  else
+    puts 'La opción que ingresaste no es válida. Vuelve a intentarlo'
   end
-  puts 'Ingresa 2 para mostrar la cantidad de stock de un producto.'
-  puts 'Ingresa 3 para mostrar los productos no registrados.'
-  puts 'Ingresa 4 para mostrar los productos con un stock menor al ingresado.'
-  puts 'Ingresa 5 para registrar un nuevo producto.'
-  puts 'Ingresa 6 para salir.'
-
-  menu = gets.chomp.to_i
-
-  puts case menu
-       when 1
-         puts '1'
-       when 2
-         file = File.open('productos.txt', 'r')
-       when 3
-         puts '3'
-       when 4
-         puts '4'
-       when 5
-         puts '5'
-       when 6
-         puts 'Salir'
-         exit
-       else
-         puts 'La opción que ingresaste no es válida. Vuelve a intentarlo'
-       end
 end
-end
-
-print menu_inventario
-
-=begin
-
-SUB-MENU OPCIÓN 1
-
- 
-
-=end 
